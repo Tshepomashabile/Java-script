f39dcac1e73f68ce1efbec42ac05e45e4ab5c022
Tshepomashabile <105448997+Tshepomashabile@users.noreply.github.com>
2022-06-23 12:05:42 +0200

    Initial commit

---
+++ java.js
@@ -0,0 +1,15 @@
+class Main {
+  public static main(String[] 'args') 
+  {
+    Scanner input = new Scanner (System.in);
+    System.out.print("Input your name: ");
+    String fname = input.next();
+    System.out.print("Input your height: ");
+    String lname = input.next();
+    System.out.print("Input your Country: ");
+    String Cname = input.next();
+    System.out.println();
+    System.out.println("fname+" +lname +Cname');
+  }
+}
+   
